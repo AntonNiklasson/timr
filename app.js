b2cd33ef979238892dd9f9ba2b578b0a69feac0b
Anton Niklasson <niklasson.anton@gmail.com>
2016-08-31 15:55:05 +0200

    Reseting properly when done. Renamed stuffs to timer*.

--- app.js
+++ app.js
@@ -1,29 +1,31 @@
 window.onload = function() {
-	var countdown = null;
-	var countdownSeconds = 0;
-	var countdownPaused = false;
 
-	// Grab the input and the button.
-	var input = document.querySelector('input');
+	// The interval ID.
+	var timerID = null;
 
-	input.addEventListener('input', onInputInput);
-	input.addEventListener('focus', onInputFocus);
-	input.addEventListener('keypress', onInputKeypress);
-		
+	// The amount of seconds remaining in the current timer.
+	var timerSecondsRemaining = 0;
+
+	// Is the timer currently paused?
+	var timerIsPaused = false;
+
+	// Attach various event listeners.
+	attachListeners();
+
+	function attachListeners() {
+		var input = document.querySelector('input');
+		input.addEventListener('focus', onInputFocus);
+		input.addEventListener('keypress', onInputKeypress);
+	}
+
 	function onInputKeypress(e) {
 		if(e.keyCode === 13) {
-			console.log('enter');
-			startCountdown();
+			startTimer();
 		}
 	}
 
 	function onInputFocus() {
-		console.log('focus');
-		countdownPaused = true;
-	}
-
-	function onInputInput() {
-
+		timerIsPaused = true;
 	}
 
 	/***
@@ -35,7 +37,7 @@ window.onload = function() {
 	* 1h
 	***/
 	function readableStringToSeconds(input) {
-		var timestamp = 0;
+		var totalSeconds = 0;
 		var secondsPattern = /[0-9]+s/i;
 		var minutesPattern = /[0-9]+m/i;
 		var hoursPattern   = /[0-9]+h/i;
@@ -45,7 +47,7 @@ window.onload = function() {
 		if(match != null) {
 			var str = match[0];
 			var hours = str.substring(0, str.length - 1);
-			timestamp += hours * 3600;
+			totalSeconds += hours * 3600;
 			input = input.substring(match.index + str.length);
 		}
 
@@ -54,7 +56,7 @@ window.onload = function() {
 		if(match != null) {
 			var str = match[0];
 			var minutes = str.substring(0, str.length - 1);
-			timestamp += minutes * 60;
+			totalSeconds += minutes * 60;
 			input = input.substring(match.index + str.length);
 		}
 
@@ -63,28 +65,29 @@ window.onload = function() {
 		if(match != null) {
 			var str = match[0];
 			var seconds = str.substring(0, str.length - 1);
-			timestamp += seconds * 1; // Watch out for this being appended as a string instead of added as a number.
+
+			// TODO: Don't assume data types.
+			totalSeconds += seconds * 1;
+
 			input = input.substring(match.index + str.length);
 		}
 
-		return timestamp;
+		return totalSeconds;
 	}
 
 	function secondsToReadableString(seconds) {
 		var readableString = '';
 		var hours = 0;
 		var minutes = 0;
-		var hourInSeconds = 3600;
-		var minuteInSeconds = 60;
 
-		while(seconds > hourInSeconds) {
+		while(seconds > 3600) {
 			hours++;
-			seconds -= hourInSeconds;
+			seconds -= 3600;
 		}
 
-		while(seconds > minuteInSeconds) {
+		while(seconds > 60) {
 			minutes++;
-			seconds -= minuteInSeconds;
+			seconds -= 60;
 		}
 
 		if(hours > 0)
@@ -98,42 +101,52 @@ window.onload = function() {
 	}
 
 	/***
-	 * Start a countdown with `amount` number of milliseconds.
+	 * Start a timer with `amount` number of milliseconds.
 	 ***/
-	function startCountdown() {
-		countdownPaused = false;
+	function startTimer() {
+		timerIsPaused = false;
 		var input = document.querySelector('input');
 
 		input.blur();
 
-		if(!countdown)
-			countdown = setInterval(countdownTick, 1000);
+		if(!timerID) {
+			timerID = setInterval(timerTick, 1000);
+		}
 	};
 
-	function pauseCountdown() {
-		countdownPaused = true;	
+	function pauseTimer() {
+		timerIsPaused = false;
 	};
 
-	function countdownTick() {
+	function timerTick() {
 		var input = document.querySelector('input');
 		var seconds = readableStringToSeconds(input.value);
 
 		if(seconds === 0) {
-			clearInterval(countdown);
-			countdownDone();
-		} else if(!countdownPaused) {
+			clearInterval(timerID);
+			timerDone();
+			reset();
+		} else if(!timerIsPaused) {
 			seconds -= 1;
 			input.value = secondsToReadableString(seconds);
 		}
 	}
 
-	function countdownDone() {
+	function timerDone() {
 		Notification.requestPermission()
 			.then(function(result) {
 				if(result === 'granted') {
-					var notification = new Notification('Done!');
+					var notification = new Notification('Timr', {
+						body: 'Done!'
+					});
 				}
 			});
+	}
+
+	function reset() {
+		timerID = null;
+		timerSecondsRemaining = 0;
+		timerIsPaused = false;
 	}
 }
 
